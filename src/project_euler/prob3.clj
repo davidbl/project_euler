ca0cc5d49066be5ae4654973ef6cb82605360c93
David Blackmon <davidkblackmon@gmail.com>
2012-05-06 15:38:34 -0400

    a more efficient primality test

--- src/project_euler/prob3.clj
+++ src/project_euler/prob3.clj
@@ -1,7 +1,9 @@
 (ns project_euler.prob3)
 
 (defn is_prime?[num]
-  (zero? (count (filter (fn[x] (zero? (rem num x))) (range 2 num))))
+  (and (pos? (rem num 2))
+    (zero? (count (filter (fn[x] (zero? (rem num x))) (range 3 (Math/sqrt num) 2))))
+   )
   )
 
 (defn factors[num]
