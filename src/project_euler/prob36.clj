a5cb907c9f929bc5c73da1c78a0cf028b7869d0b
David Blackmon <davidkblackmon@gmail.com>
2012-05-11 23:02:00 -0400

    problem 36

---
+++ src/project_euler/prob36.clj
@@ -0,0 +1,14 @@
+(ns project_euler.prob36
+  (:require [project_euler.core :as euler]))
+
+(defn palindrome_string? [str]
+  (= str (clojure.string/reverse str))
+  )
+
+(defn both?[x] (and (palindrome_string? (str x))
+                          (palindrome_string? (Integer/toString x 2)))
+  )
+
+(defn prob36a[maxVal]
+  (apply + (filter both? (range 1 maxVal)))
+  )
